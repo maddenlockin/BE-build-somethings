32dd9079fc8e46dd677e947a2ce1e26c5b105bde
maddenlockin <maddenlockin@gmail.com>
2021-09-27 17:30:41 -0700

    gets-all test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,26 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+describe('REST room app', () => {
+    beforeEach(() => {
+        return setup(pool);
+    });
 
-  afterAll(() => {
-    pool.end();
-  });
+    it('gets all addresses from the api', () => {
+        return request(app)
+            .get('api/v1/restrooms/')
+            .then((res) => {
+                expect(res.body).toEqual([{
+                    address: expect.any(String),
+                    place: expect.any(String),
+                    state: expect.any(String),
+                    city: expect.any(String),
+                }]);
+            });
+    });
+
+
+    afterAll(() => {
+        pool.end();
+    });
 });
